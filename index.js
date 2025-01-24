5710ab12e0fe6ff6bbc376cd9f51f6d8a680805e
prasanth0820 <prasanth0820.k@gmail.com>
2025-01-24 06:27:51 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const qrcode = require('qrcode')
+const path = require('path')
+
+// middlewarecode --------------------------------use for gain data from the frontend to backend
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.set('views', path.join(__dirname, 'views'))
+app.use(express.static("public"))
+app.use(express.static("src"))
+
+
+
+
+app.get('/', (req, res) => {
+    res.render('index')
+
+})  
+app.get('/Qr-code', (req, res) => {
+    res.render('Qr-code')
+
+})
+app.post('/imgdata', (req, res) => {
+    console.log(req.body.qrdata);
+    const qrcodedata = req.body.qrdata
+    qrcode.toDataURL(qrcodedata, (err, data) => {
+        console.log(data);
+        res.render('qr-code',{"transfer":data})
+
+
+    })
+
+})
+
+app.listen(10000)
